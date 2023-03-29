cdf6c8879558f4e04439cc6896fd03ed0ca1fe8a
wisman101 <lewiskimani18@gmail.com>
2023-03-29 21:47:15 +0300

    update: program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using ValenciaBot.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 builder.Services.AddDbContext<MainContext>(
