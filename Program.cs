4c679803bc01cce45871513dfe723223a0204490
wisman101 <lewiskimani18@gmail.com>
2023-04-16 12:14:01 +0300

    update: program.cs

--- Program.cs
+++ Program.cs
@@ -11,10 +11,7 @@ if(builder.Environment.EnvironmentName == "Development")
 }
 else
 {
-    builder.Configuration.AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true)
-        .AddAzureAppConfiguration(options => {
-                    options.Connect(Environment.GetEnvironmentVariable("ConnectionString"));
-                });
+    builder.Configuration.AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true);
 }
 
 
